fa32609507889e1653b68366dde7f36ab9c5c982
michaelsatumba <mjsatumba@gmail.com>
2021-09-21 11:47:27 -0700

    hello

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "./Card";
+import emojipedia from "../emojipedia";
+
+function createCard(emojipedia) {
+  return (
+    <Card
+      key={emojipedia.id}
+      emoji={emojipedia.emoji}
+      name={emojipedia.name}
+      meaning={emojipedia.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(createCard)}</dl>
+    </div>
+  );
+}
+
+export default App;
